0000cde7d380452032438c4d6fdeec6f2e67dd82
web-lizzard <czarnous.adrian@gmail.com>
2024-03-17 00:22:17 +0100

    test: test GET product enpoint

--- src/tests/e2e/test_product_api.py
+++ src/tests/e2e/test_product_api.py
@@ -1,4 +1,13 @@
+import pytest
 from fastapi import status
+from server.products.utils import convert_product_to_schema
+from shop.product.infrastructure import ProductBuilder
+
+@pytest.fixture
+def expected_response(product_model):
+    return convert_product_to_schema(ProductBuilder(product_model).build()).model_dump()
+
+
 
 async def test_get_products_returns_list(client, session_maker_override, product_model): 
     response =  client.get('/products')
@@ -7,7 +16,8 @@ async def test_get_products_returns_list(client, session_maker_override, product
     assert len(response.json()) == 1
 
 
-async def test_get_product_returns_product(client, session_maker_override, product_model):
+async def test_get_product_returns_product(client, session_maker_override, product_model, expected_response):
     response = client.get('/products/'+ product_model.id)
 
     assert response.status_code == status.HTTP_200_OK
+    assert response.json() == expected_response
